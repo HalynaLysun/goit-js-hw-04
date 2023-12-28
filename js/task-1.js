c13cf36a655f80542b906a780a6c403769aa394f
HalynaLysun <125215419+HalynaLysun@users.noreply.github.com>
2023-12-28 14:43:35 +0200

    task-1

--- js/task-1.js
+++ js/task-1.js
@@ -1,5 +1,39 @@
 'use strict'
 
+function isEnoughCapacity(products, containerSize) {
+
+    let totalProducts = 0
+
+    for (const product in products) {
+        totalProducts += product.name
+        if (totalProducts >= containerSize) {
+            return true
+        } else {
+            return false
+            } 
+    }
+
+}
+
+console.log(
+  isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8)
+); // true
+
+console.log(
+  isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12)
+); // false
+
+console.log(
+  isEnoughCapacity({ apples: 1, lime: 5, tomatoes: 3 }, 14)
+); // true
+
+console.log(
+  isEnoughCapacity({ apples: 18, potatoes: 5, oranges: 2 }, 7)
+); // false
+
+
+
+
 //Напишіть код, який запитуватиме
 //логін за допомогою prompt і логувати результат
 //в консоль браузера
@@ -15,27 +49,27 @@
 // const userRule = prompt("Введіть логін");
 
 
-    //  switch userPassword {
-    // case "Я головний":
-    //     console.log("Добрий день!");
-    //     break;
-    // default:
-    //     console.log("Невірний пароль!");   
+//     //  switch userPassword {
+//     // case "Я головний":
+//     //     console.log("Добрий день!");
+//     //     break;
+//     // default:
+//     //     console.log("Невірний пароль!");   
 
-const userRule = prompt("Введіть логін");
-
-
-switch (userRule) {
-    case "Адмін":
-        console.log(prompt("Введіть пароль") === "Я головний" ? console.log("Добрий день!") : console.log("Невірний пароль!"));
-        break;
-    case "":
-        console.log("Скасовано");
-        break;
-    default:
-        console.log("Я вас не знаю");
-
-}
+// const userRule = prompt("Введіть логін");
 
 
-console.log(userRule)
+// switch (userRule) {
+//     case "Адмін":
+//         console.log(prompt("Введіть пароль") === "Я головний" ? console.log("Добрий день!") : console.log("Невірний пароль!"));
+//         break;
+//     case "":
+//         console.log("Скасовано");
+//         break;
+//     default:
+//         console.log("Я вас не знаю");
+
+// }
+
+
+// console.log(userRule)
